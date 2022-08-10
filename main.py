27ccf532430d4147a0e642b6883a46f8a78f6027
Francisco Cardo <pnchocardo@gmail.com>
2022-08-10 12:13:18 -0300

    inicializo preguntados

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ if __name__ == "__main__":
             numero_random_2 = random.randint(1, 20)
             if numero_random_1 == 1:
                 preguntas_deporte (numero_random_1,numero_random_2,correctos,incorrectos)
+                numero_random_1 = random.randint(1, 6)
+                numero_random_2 = random.randint(1, 20)
             elif numero_random_1 == 2:
                 preguntas_deporte(numero_random_1, numero_random_2, correctos, incorrectos)
+                numero_random_1 = random.randint(1, 6)
+                numero_random_2 = random.randint(1, 20)
